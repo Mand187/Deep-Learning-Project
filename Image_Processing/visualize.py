88b350c18a1fc45f59f15df6c2f6567f3b4e0499
jaskinkabir <jaskinkabir@gmail.com>
2025-04-19 18:07:47 -0400

    Edited visualization code

--- Image_Processing/visualize.py
+++ Image_Processing/visualize.py
@@ -3,14 +3,22 @@ from ultralytics import YOLO
 import time
 
 path = 'dataset/cars-1m.mp4'
+save_path = 'cars-1m-processed'
 
 model = YOLO('yolo12x.pt').to('cuda', non_blocking=True)  # load a model from file
+start = time.perf_counter()
 results = model.track(
     source = path,
-    show=False,
     save=True,
+    save_txt=True,
     half=True,
     stream=False,
-    batch=256,
-    verbose=False
+    batch=1024,
+    verbose=True,
+    visualize = True,
+    line_width = 1,
+    font_size = 8,
+
 )
+duration = time.perf_counter() - start
+print(f"Duration: {duration:.2f} seconds")
